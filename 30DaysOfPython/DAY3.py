c6d4136ea2d43a5f3ade1b9719f050fbd5ca4434
lincou <103207305+lincou@users.noreply.github.com>
2024-12-23 18:33:30 +0800

    DAY3 Update

---
+++ 30DaysOfPython/DAY3.py
@@ -0,0 +1,54 @@
+# #DAY3
+
+# print(True)
+# print(False)
+# print(type(True))
+# x = 16
+# x//=3  #整除
+# print(x)
+
+# #除了上述比较运算符之外，Python 还使用：
+
+# #is: 如果变量相等，返回 True(x is y)
+# #is not: 如果变量不相等，返回 True(x is not y)
+# #in: 如果列表包含某变量，返回 True(x in y)
+# #not in: 如果列表不包含某变量(x in y)
+
+# print('A in Asabeneh', 'A' in 'Asabeneh') # True - 字符串中含有元素 A
+# print('B in Asabeneh', 'B' in 'Asabeneh') # False - 没有大写字母 B
+# print('coding' in 'coding for all') # True - 因为 coding 都在 'coding for all' 中
+# print('a in an:', 'a' in 'an')      # True
+# print('4 is 2 ** 2:', 4 is 2 ** 2)   # True
+
+# print(3 > 2 and 4 > 3)
+
+
+# #计算三角形面积
+# base= float(input('Enter the base of the triangle: '))
+# height =float(input('Enter the height of the triangle: '))
+# print("The area of the triangle is:", 0.5*base*height)
+
+# #计算三角形的周长，注意提示用户
+# theFirstside=int(input('PLease input the trangle the first side length:'))
+# theSecondside=int(input('Please input the trangle the second side length:'))
+# theThirdside=int(input('Please input the trangle the third side length:'))
+# if theFirstside+theSecondside>theThirdside and theFirstside+theThirdside>theSecondside and theSecondside+theThirdside>theFirstside:
+#     print('The perimeter of the triangle is:', theFirstside+theSecondside+theThirdside)
+# else:
+#     print('The triangle is not valid')
+
+# #计算矩形得面积和周长
+# rectangleLength=int(input('Please input the rectangle length:'))
+# rectangleWidth=int(input('Please input the rectangle width:'))
+# print('The rectangle area is:', rectangleLength*rectangleWidth)
+# print('The rectangle perimeter is:', 2*(rectangleLength+rectangleWidth))
+
+# #计算圆的面积和周长
+# pi=3.14159
+# radius=float(input('Please input the radius of the circle:'))
+# print('The area of the circle is:', pi*radius**2)
+# print('The circumference of the circle is:', 2*pi*radius)
+
+#计算y=2x-2的斜率、x截距和y截距
+y=2*x-2
+
