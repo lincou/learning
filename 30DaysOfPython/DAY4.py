5eebc17cb191e4812d2c42f834b7ae177f04dfe5
lincou <103207305+lincou@users.noreply.github.com>
2024-12-27 18:22:01 +0800

    update

---
+++ 30DaysOfPython/DAY4.py
@@ -0,0 +1,14 @@
+# #字符串用单引号或者双引号'',""
+# #多行字符串使用三个单引号 (''') 或者三个双引号 (""") 创建
+# #字符串联
+# first_name = 'Asabeneh'
+# last_name = 'Yetayeh'
+# space = ' '
+# full_name = first_name  +  space + last_name
+# print(full_name) # Asabeneh Yetayeh
+
+# %s - 字符串 (或者任何可以用字符串表述的对象，例如数字)
+# %d - 整型
+# %f - 浮点型
+# "%.小数位数f" - 固定精度的浮点数
+
